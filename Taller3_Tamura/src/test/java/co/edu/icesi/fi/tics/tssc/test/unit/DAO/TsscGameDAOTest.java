4e4527bae8aadfa5a0092e8035110ddcfbf292bd
TakumiMay <maythermyself@gmail.com>
2020-05-12 21:17:17 -0500

    Update GameDAOTest

--- Taller3_Tamura/src/test/java/co/edu/icesi/fi/tics/tssc/test/unit/DAO/TsscGameDAOTest.java
+++ Taller3_Tamura/src/test/java/co/edu/icesi/fi/tics/tssc/test/unit/DAO/TsscGameDAOTest.java
@@ -1,8 +1,11 @@
 package co.edu.icesi.fi.tics.tssc.test.unit.DAO;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
@@ -22,43 +25,67 @@ public class TsscGameDAOTest extends TestCase {
 
 	@Autowired
 	TsscGameDAO gameDAO;
+	
+	@BeforeEach
+	public void setUp() {
+		TsscGame tsscGame = new TsscGame();
+		tsscGame.setAdminPassword("1234");
+		tsscGame.setGuestPassword("1234");
+		tsscGame.setName("Sudoku");
+		tsscGame.setScheduledDate(LocalDate.now());
+		tsscGame.setScheduledTime(LocalTime.now());
+		tsscGame.setUserPassword("1234");
+		
+		gameDAO.save(tsscGame);
+	}
 
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testSaveGame() {
 		TsscGame tsscGame = new TsscGame();
-		tsscGame.setAdminPassword("1234");
-		tsscGame.setGuestPassword("1234");
-		tsscGame.setName("hola");
-		tsscGame.setUserPassword("1234");
+		tsscGame.setAdminPassword("5678");
+		tsscGame.setGuestPassword("5678");
+		tsscGame.setName("SushiGoParty");
+		tsscGame.setScheduledDate(LocalDate.now());
+		tsscGame.setScheduledTime(LocalTime.now());
+		tsscGame.setUserPassword("5678");
+		
 		gameDAO.save(tsscGame);
+		
+		assertNotNull(gameDAO.findById(tsscGame.getId()));
 	}
 	
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testUpdateGame() {
+		TsscGame tsscGame = gameDAO.findById(1);
+		String newName = "SushiGoParty";
+		tsscGame.setName(newName);
 		
 		gameDAO.update(tsscGame);
+		
+		assertEquals(newName, gameDAO.findById(1).getName());
 	}
 
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testDeleteGame() {
 		
-		gameDAO.delete(tsscGame);
+		//gameDAO.delete(tsscGame);
 	}
 	
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testFindGameById() {
 		
-		gameDAO.findById(id);
+		//gameDAO.findById(id);
 	}
 	
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testFindAllGames() {
 		ArrayList<TsscGame> games = (ArrayList<TsscGame>) gameDAO.findAll();
+		
 //		System.out.println();
 	}
 	
@@ -66,34 +93,35 @@ public class TsscGameDAOTest extends TestCase {
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testFindGameByName() {
 	
-		gameDAO.findByName(name);
+		//gameDAO.findByName(name);
 	}
 	
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testFindGameByDescription() {
 		
-		gameDAO.findByDescription(description);
+		//gameDAO.findByDescription(description);
 	}
 
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testFindGameByTopicId() {
 		
-		gameDAO.findByTopicId(id);
+		//gameDAO.findByTopicId(id);
 	}
 	
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testFindGameByDateRange() {
 		
-		gameDAO.findByDateRange(scheduledDate, scheduledDate2);
+		//gameDAO.findByDateRange(scheduledDate, scheduledDate2);
 	}
 	
 	@Test
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void testFindGameByDateAndTimeRange() {
 		
-		gameDAO.findByDateAndTimeRange(scheduledDate, localTime, localTime2);
+		//gameDAO.findByDateAndTimeRange(scheduledDate, localTime, localTime2);
 	}
+	
 }
